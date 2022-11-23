082020c52375ed3649e876c4c6ebbeccb48f66d4
Artyom_Khachaturyan <artchief@gmail.com>
2022-11-23 21:36:00 +0300

    Task3 done

--- z3/Program.cs
+++ z3/Program.cs
@@ -5,3 +5,12 @@
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
 
+Console.WriteLine("Введите N ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+while(count<=num)
+{
+   Console.WriteLine(Math.Pow(count,3));
+   count++; 
+}
